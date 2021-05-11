36c81bd8e11862a1c66f5bbf6be24d3eb912ad5d
Sourav <ss12011999@gmail.com>
2021-05-11 17:20:06 +0530

    change text size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'myApp';
   color: String;
+  sizeText: number;
+  sizeString: String;
   constructor() {
     this.color = 'text-dark'
+    this.sizeText = 1
+    this.sizeString = "display-" + this.sizeText;
   }
+  changeTextSize(x: boolean) {
+    if (x) {
+      if (this.sizeText < 6) {
+        this.sizeText++
+        this.sizeString = "display-" + this.sizeText;
+      }
+    }
+    else {
+      if (this.sizeText > 1) {
+        this.sizeText--
+        this.sizeString = "display-" + this.sizeText;
+      }
+    }
+
+  }
+
   changeColor(num: Number) {
     switch (num) {
       case 0:
