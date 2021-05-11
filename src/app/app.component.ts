cdc092ef34b7f60957b2e3283cf16eebc79ab130
Sourav <ss12011999@gmail.com>
2021-05-11 11:31:25 +0530

    change color according to button pressed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  color: String;
+  constructor() {
+    this.color = 'text-dark'
+  }
+  changeColor(num: Number) {
+    switch (num) {
+      case 0:
+        this.color = 'text-primary'
+        return;
+      case 1:
+        this.color = 'text-warning'
+        return;
+      case 2:
+        this.color = 'text-success'
+        return;
+      case 3:
+        this.color = 'text-danger'
+        return;
+      default:
+        this.color = 'text-dark'
+        return;
+    }
+  }
 }
