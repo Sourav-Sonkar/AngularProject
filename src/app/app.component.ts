6a25e3dbf555875d30587f558203053c7f4b792f
Sourav <ss12011999@gmail.com>
2021-05-11 10:54:25 +0530

    Increase or decrease number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  num: number
+  color: String
+  constructor() {
+    this.num = 0
+    this.color = 'text-primary'
+  }
+
+  increase() {
+    this.num++
+    this.setColor()
+  }
+  decrease() {
+    this.num--
+    this.setColor()
+  }
+  setColor() {
+    this.num > 0 ? this.color = 'text-success' :
+      (this.num < 0 ? this.color = 'text-danger' : this.color = 'text-primary')
+  }
+
 }
